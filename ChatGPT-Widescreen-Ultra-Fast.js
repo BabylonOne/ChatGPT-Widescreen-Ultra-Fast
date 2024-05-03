10a9ff9d419e6a8b89b26a3870a6a1e17f82b04c
BabylonOne <168267265+BabylonOne@users.noreply.github.com>
2024-05-03 20:02:14 +1000

    Update ChatGPT-Widescreen-Ultra-Fast.js - Add bookmark activation method to comments

--- ChatGPT-Widescreen-Ultra-Fast.js
+++ ChatGPT-Widescreen-Ultra-Fast.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Widescreen Ultra Fast (Simple one line CSS edit, No-Install URL method included)
 // @id           ChatGPT_Widescreen_Ultra_Fast@https://github.com/BabylonOne/ChatGPT-Widescreen-Ultra-Fast
-// @version      2024.5.1.0
+// @version      2024.5.2.0
 // @match        *://chat.openai.com/*
 // @author       Alex Yam
 // @license      MIT; https://spdx.org/licenses/MIT.html
@@ -21,17 +21,24 @@
 // ==/UserScript==
 
 /*
-/ No-Install URL method:
-/     If you don't want to install a script, here is a No-Install URL method,
-/     On your ChatGPT chat page, paste the following URL into your URL bar and press enter:
+/ *** No-Install One-Click Bookmark Activation method ***
 /
-/         javascript:document.styleSheets[0].insertRule('.text-token-text-primary>div>div{max-width:100%!important}',0);
+/ If you don't want to install a script, create a bookmark with the following URL:
 /
-/    Chat boxes should instantly become max width upon pressing enter (if "javascript:" is not removed from the URL)
+/     javascript:document.styleSheets[0].insertRule('.text-token-text-primary>div>div{max-width:100%!important}',0);
 /
-/    Note:
-/        Some browsers automatically remove the "javascript:" from the URL for security reason,
-/        you may need to manually type or paste "javascript:" at the front of the URL, before pressing Enter.
+/ Click the bookmark and ChatGPT chat boxes should expand instantly.
+*/
+
+/*
+/ *** No-Install URL Copy and Paste Activation method ***
+/
+/ You can also copy and paste the same URL directly into the URL bar and press enter:
+/
+/     javascript:document.styleSheets[0].insertRule('.text-token-text-primary>div>div{max-width:100%!important}',0);
+/
+/ Note: Some browsers remove the "javascript:" from the URL for security reasons, you may need to manually input "javascript:"
+/       at the start of the URL before pressing Enter. ChatGPT chat boxes should expand instantly upon pressing Enter.
 */
 
 /*
