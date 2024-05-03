7503ce54b00e77e06059545801909be10c3198bf
BabylonOne <168267265+BabylonOne@users.noreply.github.com>
2024-05-04 01:15:52 +1000

    Update ChatGPT-Widescreen-Ultra-Fast.js
    Refine @description and comments for greasyfork release

--- ChatGPT-Widescreen-Ultra-Fast.js
+++ ChatGPT-Widescreen-Ultra-Fast.js
@@ -1,16 +1,14 @@
 // ==UserScript==
-// @name         ChatGPT Widescreen Ultra Fast (Simple one line CSS edit, No-Install URL method included)
+// @name         ChatGPT Widescreen Ultra Fast
 // @id           ChatGPT_Widescreen_Ultra_Fast@https://github.com/BabylonOne/ChatGPT-Widescreen-Ultra-Fast
-// @version      2024.5.2.0
+// @version      2024.5.3.0
 // @match        *://chat.openai.com/*
 // @author       Alex Yam
 // @license      MIT; https://spdx.org/licenses/MIT.html
-// @description  Simplicity is the ultimate sophistication. Don't use 10 pages of JavaScript when you can use one line. Don't use JavaScript when you can use pure CSS. Don't wait for elements to show when you don't need to. No fancy JavaScript window.onload(), addEventListener("DOMContentLoaded"), Promise(), MutationObserver(), setTimeout(), querySelector(), or any other CPU burning and scroll jittering callback nonsense. (Also includes a No-Install URL method, read first comment in code).
+// @description  The fastest ChatGPT widescreen userscript. Just one line, you don't even have to install a script. Create a bookmark and activate with one click.
 // @namespace    https://github.com/BabylonOne
 // @homepage     https://github.com/BabylonOne/ChatGPT-Widescreen-Ultra-Fast
 // @homepageURL  https://github.com/BabylonOne/ChatGPT-Widescreen-Ultra-Fast
-// @downloadURL  https://raw.githubusercontent.com/BabylonOne/ChatGPT-Widescreen-Ultra-Fast/main/ChatGPT-Widescreen-Ultra-Fast.js
-// @updateURL    https://raw.githubusercontent.com/BabylonOne/ChatGPT-Widescreen-Ultra-Fast/main/ChatGPT-Widescreen-Ultra-Fast.js
 // @supportURL   https://github.com/BabylonOne/ChatGPT-Widescreen-Ultra-Fast/issues
 // @compatible   chrome
 // @compatible   edge
@@ -18,48 +16,69 @@
 // @compatible   opera
 // @compatible   safari
 // @grant        GM_addStyle
+// @downloadURL  https://raw.githubusercontent.com/BabylonOne/ChatGPT-Widescreen-Ultra-Fast/main/ChatGPT-Widescreen-Ultra-Fast.js
+// @updateURL    https://raw.githubusercontent.com/BabylonOne/ChatGPT-Widescreen-Ultra-Fast/main/ChatGPT-Widescreen-Ultra-Fast.js
 // ==/UserScript==
 
 /*
-/ *** No-Install One-Click Bookmark Activation method ***
+/ *** No-Install One-Click Bookmark Activation Method ***
 /
-/ If you don't want to install a script, create a bookmark with the following URL:
+/ If you don't want to install a script, create a bookmark named "Expand ChatGPT" with the following URL:
 /
 /     javascript:document.styleSheets[0].insertRule('.text-token-text-primary>div>div{max-width:100%!important}',0);
 /
-/ Click the bookmark and ChatGPT chat boxes should expand instantly.
+/ Click the bookmark and all ChatGPT chat boxes should expand instantly.
 */
 
 /*
-/ *** No-Install URL Copy and Paste Activation method ***
+/ *** No-Install URL Copy and Paste Activation Method ***
 /
 / You can also copy and paste the same URL directly into the URL bar and press enter:
 /
 /     javascript:document.styleSheets[0].insertRule('.text-token-text-primary>div>div{max-width:100%!important}',0);
 /
-/ Note: Some browsers remove the "javascript:" from the URL for security reasons, you may need to manually input "javascript:"
-/       at the start of the URL before pressing Enter. ChatGPT chat boxes should expand instantly upon pressing Enter.
+/ Note: Some browsers remove the "javascript:" from the URL for security reasons, you may need to manually enter "javascript:"
+/       in front of the URL before pressing Enter. Upon pressing Enter, all ChatGPT chat boxes should expand instantly.
 */
 
 /*
-/ Simplicity is the ultimate sophistication.
+/ *** Simplicity is the ultimate sophistication ***
 /
-/ Don't use 10 pages of JavaScript when you can use one line.
-/ Don't use JavaScript when you can use pure CSS.
-/ Don't wait for elements to show when you do not need to.
+/ * Don't use 10 pages of JavaScript when you can use one line
+/ * Don't use JavaScript when you can use pure CSS
+/ * Don't wait for elements to show when you do not need to
+/ * No addEventListener()
+/ * No find()
+/ * No forEach()
+/ * No getElementById()
+/ * No matchAll()
+/ * No MutationObserver()
+/ * No Promise()
+/ * No querySelector()
+/ * No setAttribute()
+/ * No setTimeout()
+/ * No window.onload()
+/ * No xmlHttpRequest()
+/ * No other CPU burning and scroll jittering callback nonsense
 /
-/ No fancy JavaScript window.onload(), addEventListener("DOMContentLoaded"),
-/ Promise(), MutationObserver(), setTimeout(), querySelector(), or any other
-/ CPU burning and scroll jittering callback nonsense.
+/ *** How it works ***
 /
-/ The ChatGPT interface uses TailwindCSS, each response box contains a
-/ "md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]"
-/ to set a max width on chat boxes under different screen sizes.
+/ ChatGPT uses the following TailwindCSS to limit box widths under different screen sizes:
+/     "md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem]"
 /
-/ This script inserts a max-width CSS rule with the "!important" flag to the master style sheet,
-/ to keep the chat boxes at max width before they are even created.
+/ This script inserts the following CSS rule into the master style sheet:
 /
-/ OpenAI: How about spend $100 and hire a highschool student to add a max screen width setting,
+/     .text-token-text-primary > div > div {max-width:100% !important;}`
+/
+/ It overrides TailwindCSS and keeps chat boxes at 100% max-width on all screen sizes, before they are even created.
+/
+/ JavaScript is used only to insert the CSS rule. Once the rule is inserted, it applies to all current and future
+/ chat boxes on the same page, no further JavaScript processing is required.
+/
+/ This approach functions without relying on constant page element monitoring, resulting in less rendering lag,
+/ especially in lengthy conversations and on less powerful hardware.
+/
+/ OpenAI: How about spend $100 and hire a high school student to add a max-width setting,
 / or ask ChatGPT 3.5 how to do it, it's not hard.
 */
 
